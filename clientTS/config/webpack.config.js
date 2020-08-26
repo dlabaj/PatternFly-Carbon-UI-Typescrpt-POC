1c4f6a42595457ca5e59208831cdf5ad41dab116
redallen <zallen@redhat.com>
2020-08-26 16:33:36 -0400

    close?

--- clientTS/config/webpack.config.js
+++ clientTS/config/webpack.config.js
@@ -657,6 +657,14 @@ module.exports = function(webpackEnv) {
           // The formatter is invoked directly in WebpackDevServerUtils during development
           formatter: isEnvProduction ? typescriptFormatter : undefined,
         }),
+      new webpack.NormalModuleReplacementPlugin(
+        /View.carbon/,
+        resource => {
+          if (process.env.cl === 'PATTERNFLY') {
+            resource.request = resource.request.replace('carbon', 'patternfly');
+          }
+        }
+      )
     ].filter(Boolean),
     // Some libraries import Node modules but don't use them in the browser.
     // Tell webpack to provide empty mocks for them so importing them works.
--- clientTS/src/Elements/Heading/index.ts
+++ clientTS/src/Elements/Heading/index.ts
@@ -3,5 +3,5 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from 'View'; // webpack alias will look for View.{type}  in this directory
+export * from './View.carbon'; // webpack alias will look for View.{type}  in this directory
 export * from './View.interface';
